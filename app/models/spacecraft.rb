e3375ae6238242923d27c27c8c95bc26e1fc20ed
Aniket Kaushik <aniket.k.kaushik@outlook.com>
2022-05-25 00:31:49 +0530

    spacecraft_weight_sum_less_than_payload_capacity refactored

--- app/models/spacecraft.rb
+++ app/models/spacecraft.rb
@@ -10,8 +10,14 @@ class Spacecraft < ApplicationRecord
     def spacecraft_weight_sum_less_than_payload_capacity
       launch_vehicle = LaunchVehicle.find(self.launch_vehicle_id)
       payload_capacity = launch_vehicle.payload_capacity
-      unless ((launch_vehicle.spacecrafts.sum(:weight)) + self.weight) <= payload_capacity
-        errors.add(:weight_is_more_then_payload_capacity, "Sum of Weight of Spacecraft is more then the payload capacity of the Launch Vehicle")
+      if self.id.nil?
+        unless ((launch_vehicle.spacecrafts.sum(:weight)) + self.weight) <= payload_capacity
+          errors.add(:weight_is_more_then_payload_capacity, "Sum of Weight of Spacecraft is more then the payload capacity of the Launch Vehicle")
+        end
+      else
+        unless ((launch_vehicle.spacecrafts.sum(:weight) - (Spacecraft.find(self.id)).weight) + self.weight) <= payload_capacity
+          errors.add(:weight_is_more_then_payload_capacity, "Sum of Weight of Spacecraft is more then the payload capacity of the Launch Vehicle")
+        end
       end
     end
 end
